97abce4f9bf8eddf94e3f115a48c8f020c6d5f64
Refa Andhika <refandika@gmail.com>
2024-10-24 15:06:32 +0000

    Add  POST endpoint

--- README.md
+++ README.md
@@ -8,7 +8,11 @@ Currently only for local testing. To run it in local just do `cargo run`. And th
 
 ``` wscat -c ws://127.0.0.1:8080/ws ```
 
+We can also broadcast message via `/send` POST endpoint, here a sample with curl:
+
+``` curl -X POST -H "Content-Type: application/json" -d '{"message": "Hello, I'ts A broadcast Message!"}' http://127.0.0.1:8080/send ```
+
 ---
 
 To Do:
-- Add a send message API under `/send`
+- Reorganize scripts to files
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,56 @@
-use actix_web::{middleware::Logger, web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{middleware::Logger, web, App, HttpRequest, HttpServer, HttpResponse, Responder};
 use actix_web::rt::spawn;
-use actix_ws::{Message, handle};
+use actix_ws::{Message, Session, handle};
 use futures::StreamExt;
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 use std::env;
 use log::error;
 
-async fn ws(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Responder> {
+#[derive(Deserialize, Serialize, Debug, Clone)]
+struct MessagePayload {
+    message: String
+}
+
+
+struct AppState {
+    sessions: Mutex<Vec<Session>>
+}
+impl AppState {
+    // Broadcast Message To All
+    async fn broadcast_message(&self, message: &str) {
+        println!("Broadcasting text: {message}");
+        let mut sessions = self.sessions.lock().unwrap();
+        for session in sessions.iter_mut() {
+            let _ = session.text(message).await;
+        }
+    }
+}
+
+// `/send` endpoint handler 
+async fn send_message(
+    payload: web::Json<MessagePayload>,
+    data: web::Data<Arc<AppState>>
+) -> impl Responder {
+    data.broadcast_message(&payload.message).await;
+    HttpResponse::Ok().json("Message sent")
+}
+
+
+// Websocket initialization
+async fn ws(
+    req: HttpRequest,
+    body: web::Payload,
+    data: web::Data<Arc<AppState>>
+) -> actix_web::Result<impl Responder> {
     // Handshake to websocket
     let (response, mut session, mut msg_stream) = handle(&req, body)?;
+
+    let state = data.get_ref().clone();
+    {
+        let mut sessions = state.sessions.lock().unwrap();
+        sessions.push(session.clone());
+    }
 
     // Spawn handler
     spawn(async move {
@@ -24,11 +67,13 @@ async fn ws(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Resp
                     // Reversing message
                     let reversed = msg.chars().rev().collect::<String>();
 
+                    // Return the reverse only to the sending user
                     println!("Sending reversed text: {reversed}");
                     if session.text(reversed).await.is_err() {
                         error!("Error sending message");
                     }
                 },
+                Message::Close(_) => break,
                 _ => break,
             }
         }
@@ -41,15 +86,22 @@ async fn ws(req: HttpRequest, body: web::Payload) -> actix_web::Result<impl Resp
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env::set_var("RUST_LOG", "actix_web=debug,actix_server=info");
+    env_logger::init();
+
+    let state = Arc::new(AppState {
+        sessions: Mutex::new(Vec::new())
+    });
 
     // Binding a Listener
     let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
     
-    println!("Listening on: {}", addr);
     HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
+            .app_data(web::Data::new(state.clone()))
             .route("/ws", web::get().to(ws))
+            .route("/send", web::post().to(send_message))
     })
     .bind(&addr)?
     .run()
